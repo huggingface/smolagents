277ca420bc04df6073db331d354d0b99800df254
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2025-02-05 16:46:10 +0100

    Refactor

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,16 +5,15 @@ import pytest
 from smolagents.agents import MultiStepAgent
 
 
-original_init = MultiStepAgent.__init__
+original_multi_step_agent_init = MultiStepAgent.__init__
 
 
 @pytest.fixture(autouse=True)
-def patch_multi_step_agent():
+def patch_multi_step_agent_with_suppressed_logging():
     with patch.object(MultiStepAgent, "__init__", autospec=True) as mock_init:
 
-        def init_with_verbosity(self, *args, verbosity_level=-1, **kwargs):
-            # kwargs['verbosity_level'] = -1
-            original_init(self, *args, verbosity_level=verbosity_level, **kwargs)
+        def init_with_suppressed_logging(self, *args, verbosity_level=-1, **kwargs):
+            original_multi_step_agent_init(self, *args, verbosity_level=verbosity_level, **kwargs)
 
-        mock_init.side_effect = init_with_verbosity
+        mock_init.side_effect = init_with_suppressed_logging
         yield
