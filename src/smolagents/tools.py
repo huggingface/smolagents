f4c8f931deb9e021055531b1d7537ac066d0f5e6
sysradium <sysradium@users.noreply.github.com>
2025-02-12 00:54:01 +0100

    fix tool code generation in case of multiline descriptions

--- src/smolagents/tools.py
+++ src/smolagents/tools.py
@@ -235,10 +235,10 @@ class Tool:
             from typing import Optional
 
             class {class_name}(Tool):
-                name = "{self.name}"
-                description = "{self.description}"
+                name = {json.dumps(self.name)}
+                description = {json.dumps(textwrap.dedent(self.description).strip())}
                 inputs = {json.dumps(self.inputs, separators=(",", ":"))}
-                output_type = "{self.output_type}"
+                output_type = {json.dumps(self.output_type)}
             """
             ).strip()
             import re
