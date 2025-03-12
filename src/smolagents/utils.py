c8c70c09290300561f02562c0255203654e2175a
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2025-03-12 10:34:42 +0100

    Fix instance_to_source to handle static/class methods

--- src/smolagents/utils.py
+++ src/smolagents/utils.py
@@ -348,8 +348,14 @@ def instance_to_source(instance, base_cls=None):
         name: func
         for name, func in cls.__dict__.items()
         if callable(func)
-        and not (
-            base_cls and hasattr(base_cls, name) and getattr(base_cls, name).__code__.co_code == func.__code__.co_code
+        and (
+            not base_cls
+            or not hasattr(base_cls, name)
+            or (
+                isinstance(func, staticmethod)
+                or isinstance(func, classmethod)
+                or (getattr(base_cls, name).__code__.co_code != func.__code__.co_code)
+            )
         )
     }
 
