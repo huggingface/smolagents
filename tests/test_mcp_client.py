342bcb0cf1cba5a1b773f47f41ceea15b8a9c7ba
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2025-04-18 11:11:23 +0200

    Fix tests

--- tests/test_mcp_client.py
+++ tests/test_mcp_client.py
@@ -1,4 +1,3 @@
-# TODO(@grll): add tests for the MCPClient when designed accepted.
 from textwrap import dedent
 
 import pytest
@@ -27,22 +26,22 @@ def echo_server_script():
 
 def test_mcp_client_with_syntax(echo_server_script: str):
     """Test the MCPClient with the context manager syntax."""
-    server_parameters = StdioServerParameters(command="uv", args=["run", "python", "-c", echo_server_script])
+    server_parameters = StdioServerParameters(command="python", args=["-c", echo_server_script])
     with MCPClient(server_parameters) as tools:
         assert len(tools) == 1
         assert tools[0].name == "echo_tool"
-        assert tools[0].forward({"text": "Hello, world!"}) == "Echo: Hello, world!"
+        assert tools[0].forward(**{"text": "Hello, world!"}) == "Echo: Hello, world!"
 
 
 def test_mcp_client_try_finally_syntax(echo_server_script: str):
     """Test the MCPClient with the try ... finally syntax."""
-    server_parameters = StdioServerParameters(command="uv", args=["run", "python", "-c", echo_server_script])
+    server_parameters = StdioServerParameters(command="python", args=["-c", echo_server_script])
     mcp_client = MCPClient(server_parameters)
     try:
         tools = mcp_client.get_tools()
         assert len(tools) == 1
         assert tools[0].name == "echo_tool"
-        assert tools[0].forward({"text": "Hello, world!"}) == "Echo: Hello, world!"
+        assert tools[0].forward(**{"text": "Hello, world!"}) == "Echo: Hello, world!"
     finally:
         mcp_client.disconnect()
 
@@ -50,12 +49,12 @@ def test_mcp_client_try_finally_syntax(echo_server_script: str):
 def test_multiple_servers(echo_server_script: str):
     """Test the MCPClient with multiple servers."""
     server_parameters = [
-        StdioServerParameters(command="uv", args=["run", "python", "-c", echo_server_script]),
-        StdioServerParameters(command="uv", args=["run", "python", "-c", echo_server_script]),
+        StdioServerParameters(command="python", args=["-c", echo_server_script]),
+        StdioServerParameters(command="python", args=["-c", echo_server_script]),
     ]
     with MCPClient(server_parameters) as tools:
         assert len(tools) == 2
         assert tools[0].name == "echo_tool"
         assert tools[1].name == "echo_tool"
-        assert tools[0].forward({"text": "Hello, world!"}) == "Echo: Hello, world!"
-        assert tools[1].forward({"text": "Hello, world!"}) == "Echo: Hello, world!"
+        assert tools[0].forward(**{"text": "Hello, world!"}) == "Echo: Hello, world!"
+        assert tools[1].forward(**{"text": "Hello, world!"}) == "Echo: Hello, world!"
