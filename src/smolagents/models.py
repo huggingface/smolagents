0b520c9267496d3e377f97c6f73d6ae39731378f
Nishant <nishantbaheti.it19@gmail.com>
2025-04-22 17:50:45 +0000

    inheriting LiteLLMModel class for LiteLLMRouter class to maintain OOPS concept

--- src/smolagents/models.py
+++ src/smolagents/models.py
@@ -1037,24 +1037,27 @@ class LiteLLMRouter(LiteLLMModel):
             model_id = "anthropic/claude-3-5-sonnet-20240620"
         self.model_id = model_id
         self.custom_role_conversions = custom_role_conversions
-        self._model_list = model_list
-        self._router_kwargs = router_kwargs
         flatten_messages_as_text = (
             flatten_messages_as_text
             if flatten_messages_as_text is not None
             else self.model_id.startswith(("ollama", "groq", "cerebras"))
         )
-        super().__init__(model_id=model_id, flatten_messages_as_text=flatten_messages_as_text, **kwargs)
+        super().__init__(
+            model_id=model_id,
+            flatten_messages_as_text=flatten_messages_as_text, 
+            client=self.create_client(model_list, router_kwargs), 
+            **kwargs
+        )
 
-    def create_client(self):
+    def create_client(self, model_list, router_kwargs):
         try:
             from litellm import Router
         except ModuleNotFoundError:
             raise ModuleNotFoundError(
                 "Please install 'litellm' extra to use LiteLLMModel: `pip install 'smolagents[litellm]'`"
             )
-        router_kwargs = self._router_kwargs or {}
-        router: Router = Router(model_list=self._model_list, **router_kwargs)
+        router_kwargs = router_kwargs or {}
+        router: Router = Router(model_list=model_list, **router_kwargs)
         return router
 
 
