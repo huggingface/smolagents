2e31cb501748c58d2e7c5f2b7ddb0eeefc82b536
Florian VALADE <fvalade@macbook-de-florian.daggertooth-sunfish.ts.net>
2025-05-19 02:55:22 +0200

    fix type in comment

--- src/smolagents/models.py
+++ src/smolagents/models.py
@@ -1061,7 +1061,7 @@ class LiteLLMModel(ApiModel):
             if event.choices:
                 if event.choices[0].delta is None or event.choices[0].delta.content is None:
                     if getattr(event.choices[0], "finish_reason", None):
-                        break # Stop straming if a finish reason is provided
+                        break # Stop streaming if a finish reason is provided
                     else:
                         raise ValueError(f"No content or tool calls in event: {event}")
                 else:
