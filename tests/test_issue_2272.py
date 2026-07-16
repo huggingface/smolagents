70bccae3e312e7615d772b7289c61b40dc5b6196
Jay <sallomondiei@gmail.com>
2026-07-16 00:01:44 +0000

    test(smolagents): add regression test for issue #2272
    Ref: #2272
    Signed-off-by: Jay <sallomondiei@gmail.com>

---
+++ tests/test_issue_2272.py
@@ -0,0 +1,8 @@
+**Expected behavior**
+I expected the packages to be installed in the virtual environment without any errors nice and perfect.
+
+**Environment:**
+Please complete the following information:
+- OS: Windows
+- Python version: 3.11
+- Package version: (output of `pip show smolagents`)
