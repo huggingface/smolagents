7b80ccb2de4a2efd8f9839c3416bd14b69de69db
Wa-y-n-e <ikrasilnik7@gmail.com>
2026-08-20 13:33:46 +0300

    feat: export Taskmarket tools

--- src/smolagents/__init__.py
+++ src/smolagents/__init__.py
@@ -27,6 +27,7 @@ from .models import *
 from .monitoring import *
 from .remote_executors import *
 from .serialization import *
+from .taskmarket_tools import *
 from .tools import *
 from .utils import *
 from .cli import *
